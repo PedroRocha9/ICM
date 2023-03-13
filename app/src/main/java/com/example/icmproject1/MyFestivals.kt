6717d55715b315e5fe95bef7cc2bcf37cf72bc22
Diogo Correia <digas_correia@hotmail.com>
2023-03-13 01:55:46 +0000

    added login and lineup layouts

---
+++ app/src/main/java/com/example/icmproject1/MyFestivals.kt
@@ -0,0 +1,14 @@
+package com.example.icmproject1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyFestivals : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_festivals)
+    }
+
+
+}
