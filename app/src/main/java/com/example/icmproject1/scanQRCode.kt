51eeea509ef7ed5006afdb33dc54a8a73834857a
PedroRocha9 <pedro.lebrerocha@ua.pt>
2023-03-14 16:29:23 +0000

    scan a qr code with placeholder

---
+++ app/src/main/java/com/example/icmproject1/scanQRCode.kt
@@ -0,0 +1,11 @@
+package com.example.icmproject1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class scanQRCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_qrcode)
+    }
+}
