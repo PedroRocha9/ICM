289b08475258bba3f5f2ee103c02bfe506b6e9f2
PedroRocha9 <pedro.lebrerocha@ua.pt>
2023-03-14 02:52:15 +0000

    find ur buddy sketch done; half of profile done; added a few things to register+signin

---
+++ app/src/main/java/com/example/icmproject1/FindUrBuddies.kt
@@ -0,0 +1,11 @@
+package com.example.icmproject1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindUrBuddies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_ur_buddies)
+    }
+}
