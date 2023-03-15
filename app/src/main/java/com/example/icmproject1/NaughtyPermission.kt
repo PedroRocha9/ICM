957a7fa7740b1eb6b0dc22051db410d90b878b48
PedroRocha9 <pedro.lebrerocha@ua.pt>
2023-03-15 09:01:01 +0000

    naughty permissions activity

---
+++ app/src/main/java/com/example/icmproject1/NaughtyPermission.kt
@@ -0,0 +1,11 @@
+package com.example.icmproject1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NaughtyPermission : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_naughty_permission)
+    }
+}
